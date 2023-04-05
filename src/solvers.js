a351bc6c1987325f560f70b7189811e3ce6c485d
davidguy3237 <davidtran3237@gmail.com>
2023-04-05 12:30:57 -0700

    Write pseudocode to understand how Board() works

--- src/solvers.js
+++ src/solvers.js
@@ -13,11 +13,23 @@
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
+// To use the Board() constructor, you must pass it an argument in one of the following formats:
+// 1. An object. To create an empty board of size n:
+// 		{n: <num>} - Where <num> is the dimension of the (empty) board you wish to instantiate
+//    EXAMPLE: var board = new Board({n:5})
+// 2. An array of arrays (a matrix). To create a populated board of size n:
+// 		[ [<val>,<val>,<val>...], [<val>,<val>,<val>...], [<val>,<val>,<val>...] ] - Where each <val> is whatever value you want at that location on the board
+//    EXAMPLE: var board = new Board([[1,0,0],[0,1,0],[0,0,1]])
 
+
+// board.get(n) will return the 'n' index row array of the board (assuming that instance exists)
+// board.set(key, value) will create a new key/value pair in the board object.
+//  Key should be the row index number and the value should be a row array
 
 window.findNRooksSolution = function(n) {
   var solution = undefined; //fixme
-
+  // create a new board using:
+  // var board = new Board({n:n});
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
